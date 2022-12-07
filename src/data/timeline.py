a9d44d586069b272ffdfb8ee38d2ed57988903f2
Catarina Oliveira Pires <up201907925@fe.up.pt>
2022-12-07 20:05:03 +0000

    Added timeline pretty_str()

--- src/data/timeline.py
+++ src/data/timeline.py
@@ -1,4 +1,5 @@
 """Classes to represent a timeline of posts from a user and a cached timeline."""
+from tabulate import tabulate
 from datetime import datetime, timedelta
 from src.data.username import Username
 import os
@@ -49,7 +50,13 @@ class Timeline:
             return Timeline(username, [])
 
     def pretty_str(self):
-        return f"Pretty output not implemented.\n{self.posts}"  # TODO
+        p = list(map(lambda p: {"id": p["id"],
+                                "timestamp": datetime.fromisoformat(p["timestamp"]),
+                                "content": p["content"]}, self.posts))
+
+        p = sorted(p, key=lambda x: x["timestamp"], reverse=True)
+
+        return tabulate([[post["id"], post["timestamp"].strftime("%Y-%m-%d %H:%M:%S"), post["content"]] for post in p], headers=["id", "time", "content"])
 
     def cache(self, max_posts, time_to_live=None):
         data = self.to_serializable()
